adcb2b2381b6b8f24a83152dbdc1efee4f98eb7d
AG <58998623+MuirlandOracle@users.noreply.github.com>
2020-04-27 03:18:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
+apt install zip
 touch /root/king.txt
+unzip compressed.zip
 mv koth /usr/bin
 chmod +x /usr/bin/koth
 mv koth.service /etc/systemd/system
 systemctl daemon-reload
 systemctl start koth
 systemctl enable koth
-cd .. & rm -rf python-koth-service
+cd .. && rm -rf python-koth-service
