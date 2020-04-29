b20686ab10da40deaf1165866245082c8dc7213d
AG <58998623+MuirlandOracle@users.noreply.github.com>
2020-04-29 02:27:34 +0100

    Implemented catch-all

--- koth.py
+++ koth.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app=Flask(__name__)
 
-@app.route("/")
-def king():
+@app.route("/", defaults={"path": ""})
+@app.route('/<path:path>')
+def king(path):
     with open("/root/king.txt", "r") as king:
         currentking = king.read()
     return currentking
 
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=9999)
